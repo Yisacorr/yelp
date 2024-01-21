0a0d009dba7dbd3e48d127a21b9774bdf9a8b47f
Yisacor <yisacorabebe@gmail.com>
2024-01-21 00:34:38 -0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const yelpApiKey = process.env.yelpApiKey;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Yelp Proxy Server!");
+});
+
+app.get("/api/restaurants", async (req, res) => {
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+
+  if (!latitude || !longitude) {
+    return res
+      .status(400)
+      .json({ message: "Location required" });
+  }
+
+  try {
+    const response = await axios.get(
+      "https://api.yelp.com/v3/businesses/search",
+      {
+        headers: {
+          Authorization: `Bearer ${yelpApiKey}`,
+        },
+        params: {
+          latitude: latitude,
+          longitude: longitude,
+          categories: "restaurants",
+          limit: 20,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from Yelp:", error);
+    res.status(500).json({ message: "Error fetching data from Yelp" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
