4ed28e286b96bf789145fdefeda608acf90fa2ac
Yisacor <yisacorabebe@gmail.com>
2024-07-14 15:19:46 -0700

    add cron job

--- api/cronJob.js
+++ api/cronJob.js
@@ -1,37 +1,7 @@
-const { createClient } = require("@supabase/supabase-js");
 const cron = require("node-cron");
 
-// Initialize Supabase client
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error(
-    "Supabase URL and Key must be provided as environment variables"
-  );
-  process.exit(1);
-}
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-// Function to perform the keep-alive task
-const keepAlive = async () => {
-  try {
-    console.log("Executing keep-alive query...");
-    // This can be any simple query that keeps the database active
-    const { data, error } = await supabase
-      .from("experiences")
-      .select("*")
-      .limit(1);
-
-    if (error) {
-      console.error("Error keeping the database alive:", error);
-    } else {
-      console.log("Keep-alive query successful:", data);
-    }
-  } catch (error) {
-    console.error("Error executing keep-alive query:", error);
-  }
+const keepAlive = () => {
+  console.log("Running the keep-alive task...");
 };
 
 // Schedule the task to run every 3 days
